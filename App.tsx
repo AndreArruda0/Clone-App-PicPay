89cca7234e50efe1a9316e1d898f253ff1a94fdb
André Galdino Arruda <andre-arruda@outlook.com.br>
2020-06-13 14:39:32 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
